dde7663c87898676448f354fed899e3d2d8627e5
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-04-20 23:06:36 +0900

    feat: Study about escaping closure

--- Swift/Closure/Closure01.playground/Contents.swift
+++ Swift/Closure/Closure01.playground/Contents.swift
@@ -87,3 +87,77 @@ let third2: Int = incrementByTwo2()
 let ten: Int = incrementByTen()
 let twenty: Int = incrementByTen()
 let thirty: Int = incrementByTen()
+
+/*
+ 탈출 클로저
+ */
+typealias VoidVoidClosure = () -> Void
+let firstClosure: VoidVoidClosure = {
+    print("Closure A")
+}
+let secondClosure: VoidVoidClosure = {
+    print("Closure B")
+}
+
+//first와 second 매개변수 클로저는 함수의 반환 값으로 사용될 수 있으므로 탈출 클로저
+func returnOneClosure(first: @escaping VoidVoidClosure, second: @escaping VoidVoidClosure, shouldReturnFirstClosure: Bool) -> VoidVoidClosure
+{
+    //전달 인자로 전달받은 클로저를 함수 외부로 다시 반환하기 때문에 함수를 탈출하는 클로저
+    return shouldReturnFirstClosure ? first : second
+}
+
+//함수엣서 반환한 클로저가 함수 외부의 상수에 저장
+let returnedClosure: VoidVoidClosure = returnOneClosure(first: firstClosure, second: secondClosure, shouldReturnFirstClosure: true)
+
+returnedClosure()   //Closure A
+
+var closures: [VoidVoidClosure] = []
+
+// closure 매개변수 클로저는 함수 외부의 변수에 저장될 수 있으므로 탈출 클로저
+func appendClosure(closure: @escaping VoidVoidClosure)
+{
+    // 전달인자로 전달받은 클로저가 함수 외부의 변수 내부에 저장되므로 함수를 탈출
+    closures.append(closure)
+}
+
+//타입 내부 메서드의 매개변수 클로저에 @escaping 키워드를 사용하여 탈출 클로저임을 명시한 경우, 클로저 내부에서 해당 타입의 프로퍼티나 메서드, 서크스크립트 등에 접근하려면 self 키워드를 명시적으로 사용해야함
+//비탈출 클로저의 경우 self 키워드 사용은 선택사항
+
+func functionWithNonescapeClosure(closure: VoidVoidClosure)
+{
+    closure()
+}
+
+func functionWithEscapingClosure(completionHandler: @escaping VoidVoidClosure) -> VoidVoidClosure
+{
+    return completionHandler
+}
+
+class SomeClass
+{
+    var x = 10
+    
+    func runNoescapeClosure()
+    {
+        // 비탈출 클로저에서 self 키워드 사용은 선택 사항
+        functionWithNonescapeClosure {
+            x = 200
+        }
+    }
+    
+    func runEscapingClosure() -> VoidVoidClosure {
+        //탈출 클로저에서는 명시적으로 self를 사용해야 함
+        return functionWithEscapingClosure {
+            self.x = 100
+        }
+    }
+}
+
+let instance: SomeClass = SomeClass()
+
+instance.runNoescapeClosure()
+print(instance.x)
+
+let returnedEscapeClosure: VoidVoidClosure = instance.runEscapingClosure()
+returnedEscapeClosure()
+print(instance.x)
