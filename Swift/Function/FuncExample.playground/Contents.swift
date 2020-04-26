63728bc8b94aa91476636ea470f9aed1d1f2049f
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-04-26 17:27:34 +0900

    feat: Study about Variable Parameter and inout parameter

--- Swift/Function/FuncExample.playground/Contents.swift
+++ Swift/Function/FuncExample.playground/Contents.swift
@@ -49,5 +49,41 @@ levelStructInstance.levelDown()
 levelStructInstance.jumpLevel(to: 100)
 
 /*
- Type
+ 가변 매개변수를 가지는 함수의 정의와 사용
  */
+func sayHelloToFriends(me: String, friends names: String...) -> String
+{
+    var result: String = ""
+    
+    for friend in names
+    {
+        result += "Hello \(friend)!" + " "
+    }
+    
+    result += "I'm " + me + "!"
+    return result
+}
+print(sayHelloToFriends(me: "kyo", friends: "Johanson", "Jay", "Wizplan"))
+print(sayHelloToFriends(me: "kyo"))
+
+/*
+ 입출력 매개변수를 가지는 함수의 정의와 사용
+ */
+var numbers: [Int] = [1, 2, 3]
+
+func nonReferenceParameter(_ arr: [Int])
+{
+    var copiedArr: [Int] = arr
+    copiedArr[1] = 1
+}
+
+func referenceParameter(_ arr: inout [Int])
+{
+    arr[1] = 1
+}
+
+nonReferenceParameter(numbers)
+print(numbers[1])
+
+referenceParameter(&numbers)
+print(numbers[1])
