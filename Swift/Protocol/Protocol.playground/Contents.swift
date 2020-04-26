24e99aafcae657fe5ab30979e1a074a0ab92a2d2
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-04-26 13:36:18 +0900

    feat: [Swift] Study of protocol about Requesting Variable Method, Initializer ....

--- Swift/Protocol/Protocol.playground/Contents.swift
+++ Swift/Protocol/Protocol.playground/Contents.swift
@@ -101,3 +101,143 @@ class Message2: Sendable2, Receiveable
         return false
     }
 }
+
+/*
+ [가변 메서드 요구]
+ */
+protocol Resettable
+{
+    mutating func reset()
+}
+
+// 클래스이기 때문에 mutating 키워드를 붙이지 않아도 됨
+class Person: Resettable
+{
+    var name: String?
+    var age: Int?
+    
+    func reset()
+    {
+        self.name = nil
+        self.age = nil
+    }
+}
+
+// 구조체와 열거형은 mutating 키워드를 포함해야함
+// !!!!! 만약 Resettable protocol에서 가변 메서드를 요구하지 않는다면, 값 타입의 인스턴스 내부 값을 변경하는 mutating 메서드는 구현이 불가능함 !!!!!
+struct Point: Resettable
+{
+    var x: Int = 0
+    var y: Int = 0
+    
+    mutating func reset()
+    {
+        self.x = 0
+        self.y = 0
+    }
+}
+
+enum Direction: Resettable
+{
+    case east, west, south, north, unknown
+    
+    mutating func reset()
+    {
+        self = Direction.unknown
+    }
+}
+let direct: Direction = .east
+print(direct)
+
+/*
+ [이니셜라이저 요구]
+ */
+protocol Named
+{
+    var name: String { get }
+    
+    init(name: String)
+}
+
+struct Pet: Named
+{
+    var name: String
+    
+    init(name: String)
+    {
+        self.name = name
+    }
+}
+
+class Person2: Named
+{
+    var name: String
+    
+    // 클래스는 상속이 가능하기 때문에 모든 상속받는 클래스가 프로토콜의 이니셜라이져 요구를 준수해야 하므로 required 키워드를 붙여주어야 함
+    required init(name: String)
+    {
+        self.name = name
+    }
+}
+
+final class FinalPerson: Named
+{
+    var name: String
+    
+    // final은 상속 되지 않으므로 required 키워드를 붙여주지 않아도 괜찮음
+    init(name: String)
+    {
+        self.name = name
+    }
+}
+
+/*
+ 상속받은 클래스의 이니셜라이저 요구 구현 및 재정의
+ */
+class School
+{
+    var name: String
+    
+    init(name: String)
+    {
+        self.name = name
+    }
+}
+
+class MiddleSchool: School, Named
+{
+    required override init(name: String)
+    {
+        super.init(name: name)
+    }
+}
+
+/*
+ 클래스 전용 프로토콜
+ */
+protocol Readable
+{
+    func read()
+}
+
+protocol ClassOnlyProtocol: class, Readable
+{
+    
+}
+
+class SomeClass: ClassOnlyProtocol
+{
+    func read()
+    {
+        
+    }
+}
+
+// !!! Error 발생 classonlyprotocol은 클래스 타입에만 채택이 가능
+//struct SomeStructure: ClassOnlyProtocol
+//{
+//    func read()
+//    {
+//
+//    }
+//}
---
+++ Swift/Protocol/Protocol2.playground/Contents.swift
@@ -0,0 +1,61 @@
+import Cocoa
+
+protocol Named
+{
+    var name: String { get }
+}
+
+protocol Aged
+{
+    var age: Int { get }
+}
+
+struct Person: Named, Aged
+{
+    var name: String
+    var age: Int
+}
+
+class Car: Named
+{
+    var name: String
+    
+    init(name: String)
+    {
+        self.name = name
+    }
+}
+
+class Truct: Car, Aged
+{
+    var age: Int
+    
+    init(name: String, age: Int)
+    {
+        self.age = age
+        super.init(name: name)
+    }
+}
+
+func celebrateBirthday(to celebrator: Named & Aged)
+{
+    print("Happy birthday \(celebrator.name)!! Now you are \(celebrator.age)")
+}
+
+let person: Person = Person(name: "hankyo", age: 30)
+celebrateBirthday(to: person)
+
+let myCar: Car = Car(name: "Boong Boong Ie")
+//celebrateBirthday(to: myCar)  // Error! Aged protocol이 없음
+
+// (클래스 & 프로토콜) 조합에서 클래스 타입은 한 타입만 조합할 수 있음
+//var someVariable: Car & Truct & Aged
+
+// Car 클래스의 인스턴스 역할고 Aged 프로토콜을 준수하는 인스턴스 할당
+var someVariable: Car & Aged
+
+// Truct 인스턴스는 Car 클래스와 Aged 프로토콜도 준수하므로 할당이 가능
+someVariable = Truct(name: "TTrust", age: 10)
+
+// Error: Car 인스턴스인 myCar는 Aged 프로토콜을 준수하지 않으므로 할당이 불가능
+//someVariable = myCar
