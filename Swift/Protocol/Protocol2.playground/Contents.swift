6d0a769c012ba7ee774b99a3aaf7f6435a0d0bde
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-04-26 15:41:54 +0900

    feat: [Swift] Add Protocol's Selectable Request

--- Swift/Protocol/Protocol2.playground/Contents.swift
+++ Swift/Protocol/Protocol2.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import Foundation
 
 protocol Named
 {
@@ -59,3 +60,51 @@ someVariable = Truct(name: "TTrust", age: 10)
 
 // Error: Car 인스턴스인 myCar는 Aged 프로토콜을 준수하지 않으므로 할당이 불가능
 //someVariable = myCar
+
+@objc protocol Moveable
+{
+    func walk()
+    @objc optional func fly()
+}
+
+// 걷기만 할 수 있는 호랑이
+class Tiger: NSObject, Moveable
+{
+    func walk()
+    {
+        print("Tiger walks")
+    }
+}
+
+// 걷고 날 수 있는 새
+class Bird: NSObject, Moveable
+{
+    func walk()
+    {
+        print("Bird walks")
+    }
+    
+    func fly()
+    {
+        print("Bird flys")
+    }
+}
+
+
+let tiger: Tiger = Tiger()
+let bird: Bird = Bird()
+
+tiger.walk()
+bird.walk()
+bird.fly()
+
+if let moveableInstance: Moveable = tiger as? Moveable
+{
+    moveableInstance.fly?()
+}
+
+if let moveableInstance: Moveable = bird
+{
+    moveableInstance.fly?()
+}
+
