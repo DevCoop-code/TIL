9e3f8bd79295af33f94d2798183603a3ad2344bb
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-10-31 00:56:21 +0900

    feat: Study about Intrinsic Content

---
+++ iOS/UI/AutoLayoutSample01/AutoLayoutSample01/myView.swift
@@ -0,0 +1,18 @@
+//
+//  myView.swift
+//  AutoLayoutSample01
+//
+//  Created by HanGyo Jeong on 2020/10/31.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class myView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+
+}
