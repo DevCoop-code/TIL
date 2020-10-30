09d56c81903b3fcab2099c6ab750948513ee2f67
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-10-31 00:33:24 +0900

    feat: Study about simple ios autolayout

---
+++ iOS/UI/AutoLayoutSample01/AutoLayoutSample01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutSample01
+//
+//  Created by HanGyo Jeong on 2020/10/30.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
