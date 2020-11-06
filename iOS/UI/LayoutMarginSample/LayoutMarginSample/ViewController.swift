83de27c534b871309d355cca1843c30afe2bfe38
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-11-06 11:00:59 +0900

    feat: Study about Size Classes & Layout Margin

---
+++ iOS/UI/LayoutMarginSample/LayoutMarginSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutMarginSample
+//
+//  Created by HanGyo Jeong on 2020/11/06.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
